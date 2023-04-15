5d1866a7bb2f2c2a027a7bf23de3eebdd2634301
Dayal Prathap <dayalprathap24171@gmail.com>
2023-04-15 11:43:53 +0400

    uwu

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-// API endpoint to handle user logins
-app.post("/login", (req, res) => {
-  const ip = req.ip; // Get user's IP address
-  console.log(`User with IP address ${ip} logged in`);
-  // Perform other actions as needed
-  res.send("Login successful");
+app.get("/", (req, res) => {
+  const ipAddress = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  console.log(`Received request from IP address ${ipAddress}`);
+  res.send("Hello, World!");
 });
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
-
-fetch("/login", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    username: "johndoe",
-    password: "password123",
-  }),
-})
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data); // "Login successful"
-  })
